8b12eadbad9388da5ec853dcbc525b191066c9e6
jako3501 <jako3501@easv365.dk>
2024-10-22 14:47:14 +0200

    add todo pre-move to component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,11 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
@@ -22,20 +18,23 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <style scoped>
 header {
-  line-height: 1.5;
-  max-height: 100vh;
+  display: flex;
+  place-items: center;
+  padding-right: calc(var(--section-gap) / 2);
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
 
 nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+  text-align: left;
+  margin-left: -1rem;
+  font-size: 1rem;
+  padding: 1rem 0;
+  margin-top: 1rem;
 }
 
 nav a.router-link-exact-active {
@@ -61,10 +60,6 @@ nav a:first-of-type {
     display: flex;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
   }
 
   header .wrapper {
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,26 +2,36 @@
 import { ref } from 'vue'
 import { useTasks } from '../modules/useTasks'
 
+
 const { tasks,
-        addTask,
-        deleteTask,
-        newTaskTitle,
-        newTaskDescription,
-        newTaskEstimatedTime,
-        newTaskStatus, } = useTasks()
+  addTask,
+  deleteTask,
+  newTaskTitle,
+  newTaskDescription,
+  newTaskEstimatedTime,
+  newTaskStatus, } = useTasks()
 </script>
 
 <template>
   <main>
 
-    <div class="todo">
+    <div class="add-todo">
 
       <input type="text" v-model="newTaskTitle" placeholder="Add a new task" />
       <input type="text" v-model="newTaskDescription" placeholder="Add a new task description" />
       <input type="text" v-model="newTaskEstimatedTime" placeholder="Add time estimate" />
-      <input type="text" v-model="newTaskStatus" placeholder="Add a task status" />
-      <button @click="addTask">Add Task</button>
 
+      <select v-model="newTaskStatus" required name="status" id="status">
+        <option value="" selected disabled hidden>Status</option>
+        <option value="todo">Todo</option>
+        <option value="in-progress">In Progress</option>
+        <option value="completed">Completed</option>
+      </select>
+
+      <button @click="addTask">Add Task</button>
+    </div>
+
+    <div class="canban-container">
       <ul>
         <li v-for="task in tasks" :key="task">
           id: {{ task.id }}
@@ -32,8 +42,55 @@ const { tasks,
           <button @click="deleteTask(task.id)">Delete me</button>
         </li>
       </ul>
-
     </div>
+
 
   </main>
 </template>
+
+<style>
+/* New todo */
+
+.add-todo {
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+  margin: 100px;
+
+}
+
+
+.add-todo input {
+  margin: 10px;
+  height: 30px;
+  padding: 0 10px;
+  border-radius: 20px;
+  border: none;
+  outline: none;
+}
+
+/* Status dropdown menu */
+.add-todo select {
+  margin: 10px;
+  height: 30px;
+  padding: 0 10px;
+  border-radius: 20px;
+  border: none;
+  outline: none;
+  color: #000000;
+}
+
+/* hide placeholder "Status" */
+.add-todo select::invalid {
+  color: #a3a3a3;
+}
+
+.add-todo button {
+  height: 30px;
+  margin: 10px;
+  padding: 0 10px;
+  border-radius: 20px;
+  border: none;
+  outline: none;
+}
+</style>
